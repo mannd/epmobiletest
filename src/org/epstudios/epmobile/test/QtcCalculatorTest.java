23c137ca1cc2701e7474335555c50d1d642202c1
David Mann <mannd@epstudiossoftware.com>
2011-09-10 10:50:54 -0400

    added tests for all QTc formulae

--- src/org/epstudios/epmobile/test/QtcCalculatorTest.java
+++ src/org/epstudios/epmobile/test/QtcCalculatorTest.java
@@ -17,6 +17,19 @@ public class QtcCalculatorTest extends TestCase {
 	public void testCalculate() {
 		assertEquals(0, QtcCalculator.calculate(0, 0, QtcFormula.BAZETT));
 		assertEquals(400, QtcCalculator.calculate(1000, 400, QtcFormula.BAZETT));
+		assertEquals(460, QtcCalculator.calculate(863, 427, QtcFormula.BAZETT));
+	}
+	
+	public void testCalculateFridericia() {
+		assertEquals(0, QtcCalculator.calculate(0, 0, QtcFormula.FRIDERICIA));
+		assertEquals(400, QtcCalculator.calculate(1000, 400, QtcFormula.FRIDERICIA));
+		assertEquals(448, QtcCalculator.calculate(863, 427, QtcFormula.FRIDERICIA));
+	}
+	
+	public void testCalculateSagie() {
+		assertEquals(154, QtcCalculator.calculate(0, 0, QtcFormula.SAGIE));
+		assertEquals(400, QtcCalculator.calculate(1000, 400, QtcFormula.SAGIE));
+		assertEquals(448, QtcCalculator.calculate(863, 427, QtcFormula.SAGIE));
 	}
 
 }
